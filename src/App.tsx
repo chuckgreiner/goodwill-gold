1a2daa08ba8b2a57bada5820151b413470dfe13d
Chuck Greiner <chuck.greiner@gmail.com>
2025-11-10 11:20:02 -0500

    Initial commit with vercel.json

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+﻿export default function App() {
+  return (
+    <div className='h-screen flex items-center justify-center bg-slate-900 text-white text-3xl'>
+      Goodwill Gold is alive!
+    </div>
+  );
+}
